526d9cd02f031833601465f1d2f1ef826ab0599b
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2025-06-03 16:53:58 +0800

    poosh

--- src/app/(dashboard)/superadmin/games/battlepass/Battlepass.tsx
+++ src/app/(dashboard)/superadmin/games/battlepass/Battlepass.tsx
@@ -143,7 +143,7 @@ export default function Battlepass() {
                       
                         <TableCell className="font-medium">{item.status}</TableCell>
                         <TableCell className="font-medium">
-                            <UpdateBattlePass seasonname={item.seasonName} start={item.startDate} end={item.endDate} status={item.status} tiercount={item.tierCount} premcost={item.tierCount} grandreward={item.grandreward.name} id={item.id} />
+                            <UpdateBattlePass seasonname={item.seasonName} start={item.startDate} end={item.endDate} status={item.status} tiercount={item.tierCount} premcost={item.premiumCost} grandreward={item.grandreward.name} id={item.id} />
                         </TableCell>
 
                         {/* <TableCell className="text-right flex items-center gap-2">
