72d7d1d158d2e5b8aa0aad9c29ee3ade215a9c7b
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2026-01-17 11:31:32 +0800

    pooosh

--- src/app/(dashboard)/user/vippacks/page.tsx
+++ src/app/(dashboard)/user/vippacks/page.tsx
@@ -5,9 +5,16 @@ import TopupHistory from './Table'
 import PayPalTopUpCard from '@/components/cards/TopupCard'
 import { useGetVipPacks } from '@/client_actions/user/vip-packs'
 import { StoreCard } from './vip-card'
+import { useUserData } from '@/client_actions/user/dashboard/dashboard'
+import useCharacterStore from '@/hooks/character'
+import { Wallet } from 'lucide-react'
 
 export default function page() {
   const {data} = useGetVipPacks()
+  const { characterid } = useCharacterStore()
+  
+  const { data: wallet, isLoading } = useUserData(characterid)
+  
 
   return (
     <Userlayout>
@@ -17,28 +24,33 @@ export default function page() {
                           <p className=' text-lg font-semibold'>VIP Packs</p>
                       </div>
 
-                      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-6 p-4 w-full">
+                      
+                      <div className=' w-full flex flex-col gap-8 p-4'>
+                        <div className=" flex items-center justify-center w-fit min-w-[300px]">
+                        <div className="flex flex-col gap-2 w-full h-auto bg-zinc-900 p-8 rounded-md">
+                          <p className="text-sm uppercase text-amber-50">Credit Balance</p>
+                          <div className=" flex items-center gap-2 mt-2">
+                            <Wallet size={25}/>
+                            <h2 className="text-4xl text-orange-500 font-semibold">{(wallet?.wallet.find((item) => item.type === 'topupcredit')?.amount || 0).toLocaleString()}</h2>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className=' border-t-2 border-zinc-800 w-full'>
+
+                      </div>
+                      </div>
+
+                      
+                      
+
+                      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4 w-full">
                         {data?.data.map((item) => (
                           <StoreCard key={item.id} item={item} />
                         ))}
                       </div>
 
-                  <div className=' w-full grid grid-cols-1 lg:grid-cols-[700px_1fr] gap-6'>
-                      {/* <div className=' w-full max-w-[400px] bg-amber-950 h-fit rounded-md flex flex-col gap-6 items-center justify-center py-12 border-[1px] border-amber-900'>
-                        <div className=' p-4 bg-zinc-950 rounded-md'>
-                          <img src="/paypal.png" alt="" width={250} height={250} />
-                        </div>
-
-                        <button className=' py-2 px-12 rounded-md bg-yellow-500 text-sm text-amber-950 font-medium'>Connect Paypal Wallet</button>
-                      </div> */}
-
-                    {/* <PayPalTopUpCard/> */}
-
-                   
-
-
-                      {/* <TopupHistory/> */}
-                    </div>
+             
                   </div>
 
           </div>
